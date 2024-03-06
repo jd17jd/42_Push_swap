d91030b4329f66d177aa1558b6a7601d0881e5b5
jd17jd <juandieg01888@gmail.com>
2024-03-07 00:33:46 +0100

    movimientos correctos

--- src/movements/push.c
+++ src/movements/push.c
@@ -6,14 +6,13 @@
 /*   By: jvivas-g <jvivas-g@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 13:57:57 by jvivas-g          #+#    #+#             */
-/*   Updated: 2024/03/06 13:48:15 by jvivas-g         ###   ########.fr       */
+/*   Updated: 2024/03/07 00:32:07 by jvivas-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../../inc/ft_push_swap.h"
 
 //PUSH
-
 void	push(t_node **stackA, t_node **stackB)
 {
 	t_node *tmp;
@@ -25,29 +24,13 @@ void	push(t_node **stackA, t_node **stackB)
 	*stackA = (*stackA)->next;
 	if (*stackA)
 		(*stackA)->prev = NULL;
+	
 	if (*stackB)
 		(*stackB)->prev = tmp;
 	tmp->next = *stackB;
 	tmp->prev = NULL;
 	*stackB = tmp;
 }
-/* void	push(t_node **stackA, t_node **stackB)
-{
-	t_node *tmp;
-	
-	if (!*stackA)
-		return ;
-
-	tmp = *stackA;
-	*stackA = (*stackA)->next;
-	if (*stackA)
-		(*stackA)->prev = NULL;
-	//delete_node(stackA, tmp);
-
-	node_add_front(stackB, node_new(tmp->content, tmp->position));
-	
-	free(tmp);
-} */
 
 //pa. Cima de StackB a la cima de StackA
 void    pa(t_node **a, t_node **b)
